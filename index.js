0055a10002554bfe8531c85a9936ae820725ede7
Arjan van Eersel <arjan@vaneersel.eu>
2019-04-24 15:13:33 +0300

    Added connection with aeternity network

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const contractAddress ='ct_26n3got4Ezqno69Vfb5KZM8MizwEDBhLyjezALeB873pbug51j';
+const contractAddress ='ct_2qcqwwXmfLmZ3a18yvnv4p8ta9HGoyHRjCDvPMvyAqkuMRwzPD';
 var client = null;
 var memeArray = [];
 var memesLength = 0;
@@ -11,15 +11,45 @@ function renderMemes() {
   $('#memeBody').html(rendered);
 }
 
+async function callStatic(func, args, types) {
+  const calledGet = await client.contractCallStatic(contractAddress,
+  'sophia-address', func, {args}).catch(e => console.error(e));
+
+  const decodedGet = await client.contractDecodeData(types,
+  calledGet.result.returnValue).catch(e => console.error(e));
+
+  return decodedGet;
+}
+
+async function contractCall(func, args, value, types) {
+  const calledSet = await client.contractCall(contractAddress, 'sophia-address',
+  contractAddress, func, {args, options: {amount:value}}).catch(async e => {
+    const decodedError = await client.contractDecodeData(types,
+    e.returnValue).catch(e => console.error(e));
+  });
+
+  return
+}
+
+
 window.addEventListener('load', async () => {
   $("#loader").show();
 
   client = await Ae.Aepp();
 
-  const calledGet = await client.contractCallStatic(contractAddress, 'sophia-address', 'getMemesLength', {args: '()'}).catch(e => console.error(e));
-  console.log('calledGet', calledGet);
-  const decodedGet = await client.contractDecodeData('int', calledGet.result.returnValue).catch(e => console.error(e));
-  console.log('decodedGet', decodedGet.value);
+  const getMemesLength = await callStatic('getMemesLength','()','int');
+  memesLength = getMemesLength.value;
+
+  for (let i = 1; i <= memesLength; i++) {
+    const meme = await callStatic('getMeme',`(${i})`,'(address, string, string, int)');
+
+    memeArray.push({
+      creatorName: meme.value[2].value,
+      memeUrl: meme.value[1].value,
+      index: i,
+      votes: meme.value[3].value,
+    })
+  }
 
   renderMemes();
 
@@ -27,16 +57,28 @@ window.addEventListener('load', async () => {
 });
 
 jQuery("#memeBody").on("click", ".voteBtn", async function(event){
+  $("#loader").show();
+
   const value = $(this).siblings('input').val();
   const dataIndex = event.target.id;
+
+  await contractCall('voteMeme',`(${dataIndex})`,value,'(int)');
+
   const foundIndex = memeArray.findIndex(meme => meme.index == dataIndex);
   memeArray[foundIndex].votes += parseInt(value, 10);
+
   renderMemes();
+
+  $("#loader").hide();
 });
 
 $('#registerBtn').click(async function(){
-  var name = ($('#regName').val()),
-      url = ($('#regUrl').val());
+  $("#loader").show();
+
+  const name = ($('#regName').val()),
+        url = ($('#regUrl').val());
+
+  await contractCall('registerMeme',`("${url}","${name}")`,0,'(int)');
 
   memeArray.push({
     creatorName: name,
@@ -44,6 +86,8 @@ $('#registerBtn').click(async function(){
     index: memeArray.length+1,
     votes: 0
   })
-  //update and render meme
+
   renderMemes();
+
+  $("#loader").hide();
 });
